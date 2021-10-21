1448901d2789cbd07ead364c433edb67f445d61d
Travis Jankowski <torist2@protonmail.com>
2021-10-21 12:07:43 -0500

    refactoring io/data

--- client/script.js
+++ client/script.js
@@ -1,101 +1,97 @@
-// set the maximum frames per second rendered
-const fps = 60;
-// connect to server
-let socket = io();
-// input variables
-let keys = [];
-
-let mouse = {
-    x: 0,
-    y: 0,
-    click: false
+let objects; // track game objects
+const fps = 60; // set the maximum frames per second rendered
+let socket = io(); // connect to server
+let input = { // input variables
+    name: "",
+    canvas: null,
+    keys: [],
+    mouse: {
+        x: null,
+        y: null,
+        click: false
+    }
 };
-// screen variables
-let centerX = 0;
-let centerY = 0;
-// send the first ping to the server
-function init() {
-    let data = {
-        name: document.getElementById('name').value,
-        centerX: centerX,
-        centerY: centerY
-    };
-    socket.emit('initialPing', data);
+// server communication
+function init() { // send the first ping to the server
+    input.name = document.getElementById("name").value; // get the player name
+    let data = input;
+    socket.emit("initServer", data); // send initial input data to the server
+    document.getElementById("join").hidden = true; // hide join form
 }
-// recieve server data
-socket.on("serverPing", function(data) {
-    // TODO retrieve data from server
-    data = {
-        mouse: mouse,
-        keys: keys
-    };
+
+socket.on("initClient", function(data) { // receive the first output data from the server
+    output = data; // store output data
+    render(); // start rendering the game data
+    data = input;
+    socket.emit("clientPing", data); // send input back to the server
+});
+
+socket.on("serverPing", function(data) { // recieve server data
+    output = data; // store output data
+    data = input;
     socket.emit("clientPing", data);
 });
 
-function resize(canvas) { // resize the window
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+function resize() { // resize the window
+    input.canvas.width = window.innerWidth;
+    input.canvas.height = window.innerHeight;
 
-    centerX = Math.floor(canvas.width / 2);
-    centerY = Math.floor(canvas.height / 2);
+    //centerX = Math.floor(input.canvas.width / 2);
+    //centerY = Math.floor(input.canvas.height / 2);
 }
 
-function render(canvas) { // draw game objects
-    let ctx = canvas.getContext("2d");
-    window.setInterval(function () {
+function render() { // draw game objects
+    let ctx = input.canvas.getContext("2d"); // get canvas rendering context
+    window.setInterval(function () { // main game render loop
         window.requestAnimationFrame(function () {
-            ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+            ctx.clearRect(0, 0, input.canvas.width, input.canvas.height); // erase canvas to render new frame
+            for (obj in objects) // draw each game object
+                obj.draw(ctx);
         });
     }, 1000/fps);
 }
 
 // key input events
-document.onkeydown = function (e) {
-    keys.push(e.key);
+document.onkeydown = function (e) { // record key presses
+    input.keys.push(e.key.toLowerCase());
 };
 
-document.onkeyup = function (e) {
-    while (keys.includes(e.key.normalize())) {
-        keys.splice(keys.indexOf(e.key.normalize()), 1);
+document.onkeyup = function (e) { // requires further testing and optimization
+    while (input.keys.includes(e.key.toLowerCase())) {
+        input.keys.splice(input.keys.indexOf(e.key.toLowerCase()), 1);
     }
-    //while (keys.includes(e.key.toUpperCase())) {
-    //    keys.splice(keys.indexOf(e.key.toUpperCase()), 1);
-    //}
-    //while (keys.includes(e.key.toLowerCase())) {
-    //    keys.splice(keys.indexOf(e.key.toLowerCase()), 1);
-    //}
 };
 
 window.onload = function () { // initialize the canvas and input
     // get the canvas element
-    let canvas = document.getElementById("canvas");
-    resize(canvas); // size canvas to window
-    document.body.onresize = resize(canvas); // automatically resize canvas with the window
+    input.canvas = document.getElementById("canvas");
+    resize(); // size canvas to window
+    document.body.onresize = resize(); // automatically resize canvas with the window
     // mouse tracking events
-    canvas.onmousedown = function () {
-        mouse.down = true;
+    input.canvas.onmousedown = function () {
+        input.mouse.down = true;
     };
 
-    canvas.onmouseup = function () {
-        mouse.down = false;
+    input.canvas.onmouseup = function () {
+        input.mouse.down = false;
     };
 
-    canvas.onmouseenter = function (e) {
-        mouse.x = e.clientX;
-        mouse.y = e.clientY;
+    input.canvas.onmouseenter = function (e) {
+        input.mouse.x = e.clientX;
+        input.mouse.y = e.clientY;
     };
 
-    canvas.onmousemove = function (e) {
-        mouse.x = e.clientX - canvas.offsetLeft - translationX;
-        mouse.y = e.clientY - canvas.offsetTop - translationY;
+    input.canvas.onmousemove = function (e) {
+        input.mouse.x = e.clientX - input.canvas.offsetLeft - translationX;
+        input.mouse.y = e.clientY - input.canvas.offsetTop - translationY;
     };
 
-    canvas.onmouseover = function (e) {
-        mouse.x = e.clientX - canvas.offsetLeft - translationX;
-        mouse.y = e.clientY - canvas.offsetTop - translationY;
+    input.canvas.onmouseover = function (e) {
+        input.mouse.x = e.clientX - input.canvas.offsetLeft - translationX;
+        input.mouse.y = e.clientY - input.canvas.offsetTop - translationY;
     };
 
-    canvas.oncontextmenu = function (e) { // no context menu on right click
+    input.canvas.oncontextmenu = function (e) { // no context menu on right click
         e.preventDefault();
     };
 };
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let express = require("express");
+const { createContext } = require("vm");
 let app = express();
 let http = require("http").createServer(app);
 
@@ -7,42 +8,137 @@ let io = require("socket.io")(http);
 const tps = 64; // set the maximum game ticks per second
 const port = 8080; // server port
 
-let clients = []; // store active users
-
+let objects = []; // store active users
+// serve webpage to client
 app.use("/client", express.static(__dirname + "/client"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/client/index.html");
 });
+// utility functions
+function getCollision(obj1, obj2) { // detect collisions between objects
+    return (
+        obj1.x < obj2.x + obj2.w &&
+        obj1.x + obj1.w > obj2.x &&
+        obj1.y < obj2.y + obj2.h &&
+        obj1.y + obj1.h > obj2.y
+    );
+}
 
-io.on("connection", function (socket) {
+function edgeCollision(obj1, obj2) { // prevent objects from crossing each other
+    let velX = obj1.x + obj1.w / 2 - (obj2.x + obj2.w / 2);
+    let velY = obj1.y + obj1.h / 2 - (obj2.y + obj2.h / 2);
+
+    let hws = obj1.w / 2 + obj2.w / 2;
+    let hhs = obj1.h / 2 + obj2.h / 2;
+
+    let colDir = null; // value if no collision
+    if (Math.abs(velX) < hws && Math.abs(velY) < hhs) { // detect collision
+        let oX = hws - Math.abs(velX);
+        let oY = hhs - Math.abs(velY);
+        if (oX >= oY) { // vertical collision
+            if (velY > 0) { // collision on top
+                colDir = "up";
+                obj1.y += oY;
+            } else { // collision on bottom
+                colDir = "down";
+                obj1.y -= oY;
+            }
+        } else { // side collision
+            if (velX > 0) { // collision on the left side
+                colDir = "left";
+                obj1.x += oX;
+            } else { // collision on the right side
+                colDir = "right";
+                obj1.x -= oX;
+            }
+        }
+    }
+    return colDir;
+}
+
+function getFOV(player) {
+    let nearby = [];
+    let fov = {
+        x: 0,
+        y: 0,
+        w: 0,
+        h: 0
+    };
+    for (obj in objects) {
+        if (getCollision(fov, obj))
+            nearby.push(obj);
+    }
+    return nearby;
+}
+// classes/objects
+function createPlayer(obj) { // add player attributes when player connects
+    obj = {
+        ...obj,
+        type: "player",
+        x: 0,
+        y: 0,
+        w: 0,
+        h: 0,
+        ang: 0,
+        img: "",
+        draw: function(ctx) {
+            ctx.save();
+            ctx.translate(this.x, this.y);
+            ctx.rotate(this.ang);
+            ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+            ctx.restore();
+        },
+        update: function() {
+
+        }
+    };
+}
+// handle server io
+io.on("connection", function (socket) { // start communication to client
     console.log("User connected: " + socket.id);
-    clients.push(socket); // add to active users
-    socket.on("initialPing", function(data) {
-        socket.name = data.name; // set player username
-        // SEND DATA FOR NEARBY OBJECTS ONLY FOR SECURITY
-        let data = { // TODO send players and map data
-            player: socket,
+    objects.push(createPlayer(socket)); // add new player to active users
+    socket.on("initServer", function(data) {
+        socket = { // merge new data into socket
+            ...socket,
+            ...data
         };
-        socket.emit("serverPing", data);
+        // send output data to client
+        data = getFOV(socket);
+        socket.emit("initClient", data);
     });
     socket.on("clientPing", function(data) {
-        socket.input = data; // store input data
-        data = {
-            player: socket,
+        socket = { // merge new data into socket
+            ...socket,
+            ...data
         };
+        // send output data to client
+        data = getFOV(socket);
         socket.emit("serverPing", data);
     });
     socket.on("disconnect", function() {
         console.log("User disconnected: " + socket.id);
-        clients.splice(clients.indexOf(socket), 1); // remove userdata
+        objects.splice(objects.indexOf(socket), 1); // remove userdata
     });
 });
 
 http.listen(port, function () { // start the server
     console.log("Listening on port: " + port);
 });
-
+// run the game
 setInterval(function() { // server game tick
-    clients.forEach();
+    let updated = [];
+    for (obj in objects) {
+        if (obj.type == "player") {
+            obj.update();
+            obj.updated = true;
+            for (near in getFOV(obj))
+                if (near.updated === false) {
+                    near.update();
+                    near.updated = true;
+                }
+        }
+    }
+    for (obj in updated)
+        obj.updated = false;
 }, 1000/tps);
